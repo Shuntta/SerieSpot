fdf8f8fa48ab71cad66a72a11f9441d78c6cfcbf
Shuntta <d.s.batista@icloud.com>
2024-01-14 02:15:03 +0000

    botao review feito

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,16 +5,13 @@ import "swiper/swiper-bundle.css";
 import "../assets/styles/Home.css";
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
-
-
-
 const Home = () => {
   return (
     <div className="home">
       <div className="container">
         <Swiper
           modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={10}
+          spaceBetween={1}
           slidesPerView={4}
           navigation
           pagination={{ clickable: true }}
@@ -22,40 +19,71 @@ const Home = () => {
           onSlideChange={() => console.log('slide change')}
         >
           <SwiperSlide>
-            <div className="movie" id="movie1">
-              {/* Conteúdo do primeiro filme */}
-            </div>
+              <div className="tudo">
+                <div className="movie" id="movie1">
+                  {/* Conteúdo do primeiro filme */}
+                </div>
+                
+                <button>Review</button>
+              </div>
+
           </SwiperSlide>
 
           <SwiperSlide>
-            <div className="movie" id="movie2">
-              {/* Conteúdo do segundo filme */}
-            </div>
+              <div className="tudo">
+                <div className="movie" id="movie2">
+                  {/* Conteúdo do primeiro filme */}
+                </div>
+                
+                <button>Review</button>
+              </div>
+
           </SwiperSlide>
 
           <SwiperSlide>
-            <div className="movie" id="movie3">
-              {/* Conteúdo do terceiro filme */}
-            </div>
+              <div className="tudo">
+                <div className="movie" id="movie3">
+                  {/* Conteúdo do primeiro filme */}
+                </div>
+                
+                <button>Review</button>
+              </div>
+
           </SwiperSlide>
 
           <SwiperSlide>
-            <div className="movie" id="movie4">
-              {/* Conteúdo do quarto filme */}
-            </div>
+              <div className="tudo">
+                <div className="movie" id="movie4">
+                  {/* Conteúdo do primeiro filme */}
+                </div>
+                
+                <button>Review</button>
+              </div>
+
           </SwiperSlide>
 
           <SwiperSlide>
-            <div className="movie" id="movie5">
-              {/* Conteúdo do quinto filme */}
-            </div>
+              <div className="tudo">
+                <div className="movie" id="movie5">
+                  {/* Conteúdo do primeiro filme */}
+                </div>
+                
+                <button><p>Review</p></button>
+              </div>
+
           </SwiperSlide>
 
           <SwiperSlide>
-            <div className="movie" id="movie6">
-              {/* Conteúdo do sexto filme */}
-            </div>
+              <div className="tudo">
+                <div className="movie" id="movie6">
+                  {/* Conteúdo do primeiro filme */}
+                </div>
+                
+                <button>Review</button>
+              </div>
+
           </SwiperSlide>
+
           {/* Adicione mais SwiperSlides conforme necessário */}
         </Swiper>
       </div>
