728aac3bdd9e9f75b5202d967370b5d3516fb61b
Diogo Batista <d.s.batista@icloud.com>
2024-02-01 20:24:50 +0000

    Containers feitos falta o css

--- src/views/MySeries.js
+++ src/views/MySeries.js
@@ -3,12 +3,30 @@ import "../assets/styles/MySeries.css";
 const MySeries = () => {
   return (
     <div className="tudo">
-      <span className="watchedText">Watched</span>
-      <div className="containerWatched">
-        <div className="movie" id="movie1" />
-        <div className="movie" id="movie2" />
-        <div className="movie" id="movie3" />
-        <div className="movie" id="movie4" />
+      <div className="container">
+        <div className="containerWatched">
+          <span className="watchedText">Watched</span>
+          <div className="movie" id="movie1" />
+          <div className="movie" id="movie2" />
+          <div className="movie" id="movie3" />
+          <div className="movie" id="movie4" />
+        </div>
+
+        <div className="containerLiked">
+          <span className="watchedText">Like</span>
+          <div className="movie" id="movie1" />
+          <div className="movie" id="movie2" />
+          <div className="movie" id="movie3" />
+          <div className="movie" id="movie4" />
+        </div>
+
+        <div className="containerToWatch">
+          <span className="watchedText">ToWatch</span>
+          <div className="movie" id="movie1" />
+          <div className="movie" id="movie2" />
+          <div className="movie" id="movie3" />
+          <div className="movie" id="movie4" />
+        </div>
       </div>
     </div>
   );
