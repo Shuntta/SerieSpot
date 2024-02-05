e8930b100df78b82fbefb9055c1b420f7e2de5ec
Shuntta <d.s.batista@icloud.com>
2024-02-05 01:05:23 +0000

    Home feita, com API

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,90 +1,52 @@
 // Home.js
-import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import "../assets/styles/Home.css";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        // Substitua pela sua chave de API diretamente
+        const apiKey = '659b7a4b0ba0a85e50933e72a4644fa5';
+        const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`);
+        setMovies(response.data.results.slice(0, 6)); // Limita a resposta aos primeiros 5 filmes
+      } catch (error) {
+        console.error("Erro ao buscar filmes:", error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
   return (
     <div className="home">
       <div className="container">
-        
-          <Link to="/CreateReview" className="New-review">
-            New Review +
-          </Link>
+        <Link to="/CreateReview" className="New-review">New Review +</Link>
 
         <Swiper
           modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={1}
-          slidesPerView={4}
+          spaceBetween={50}
+          slidesPerView={3}
           navigation
           pagination={{ clickable: true }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
         >
-          <SwiperSlide>
-            <div className="tudo">
-              <div className="movie" id="movie1">
-                {/* Conteúdo do primeiro filme */}
+          {movies.map((movie) => (
+            <SwiperSlide key={movie.id}>
+              <div className="tudo">
+                <div className="movie" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.poster_path})` }}>
+                  {/* Aqui você pode adicionar mais informações do filme se desejar */}
+                </div>
+                <button className="Review">Review</button>
               </div>
-
-              <button className="Review">Review</button>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className="tudo">
-              <div className="movie" id="movie2">
-                {/* Conteúdo do primeiro filme */}
-              </div>
-
-              <button className="Review">Review</button>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className="tudo">
-              <div className="movie" id="movie3">
-                {/* Conteúdo do primeiro filme */}
-              </div>
-
-              <button className="Review">Review</button>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className="tudo">
-              <div className="movie" id="movie4">
-                {/* Conteúdo do primeiro filme */}
-              </div>
-
-              <button className="Review">Review</button>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className="tudo">
-              <div className="movie" id="movie5">
-                {/* Conteúdo do primeiro filme */}
-              </div>
-
-              <button className="Review">Review</button>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className="tudo">
-              <div className="movie" id="movie6">
-                {/* Conteúdo do primeiro filme */}
-              </div>
-
-              <button className="Review">Review</button>
-            </div>
-          </SwiperSlide>
-
-          {/* Adicione mais SwiperSlides conforme necessário */}
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
